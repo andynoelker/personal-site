5443ba5911bb00357cfc06c626b33489421dfb9f
andynoelker <ajnoelker@fitness-plus.net>
2018-08-17 20:06:10 -0500

    Fix Home page mobile style

--- src/shared/Home/styles.js
+++ src/shared/Home/styles.js
@@ -1,19 +1,23 @@
 const styles = theme => ({
   root: {
-    //backgroundColor: theme.palette.primary.main,
-    //backgroundColor: theme.palette.common.white,
     backgroundColor: theme.palette.primary.dark,
-    //backgroundColor: theme.palette.primary.light,
-    height: '100%',
-    width: '100%',
     padding: theme.spacing.unit * 3,
-    position: 'relative',
+    [theme.breakpoints.up('md')]: {
+      //backgroundColor: theme.palette.primary.main,
+      //backgroundColor: theme.palette.common.white,
+      //backgroundColor: theme.palette.primary.light,
+      height: '100%',
+      width: '100%',
+      position: 'relative',
+    },
   },
   center: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    [theme.breakpoints.up('md')]: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    },
   },
   headline: {
     fontSize: '60px',
