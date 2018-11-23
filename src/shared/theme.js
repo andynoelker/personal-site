9ac0fa1131572f2e4bc87566c613438e8a5dc164
andynoelker <ajnoelker@fitness-plus.net>
2018-11-23 13:11:55 -0600

    Add theme palette comments

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -1,5 +1,12 @@
+import red from '@material-ui/core/colors/red';
+import indigo from '@material-ui/core/colors/indigo';
+
 const theme = {
   drawerWidth: 240,
+  //palette: {
+    //primary: red,
+    //secondary: indigo,
+  //}
 };
 
 export default theme;
